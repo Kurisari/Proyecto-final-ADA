6ebd19c010c7ad8a456f99b3323bedab141b04b7
Gerardo <geras4215@gmail.com>
2022-11-03 15:22:15 -0600

    Comentario hola

--- proyectoFinal/mainProyecto.cpp
+++ proyectoFinal/mainProyecto.cpp
@@ -10,11 +10,11 @@ using namespace std;
 
 int tabla[F][C], tabla1[F][C], resultados[F];
 
-//Índice
-void llenado(); //Completado
-void llenadoAutomatico(); //Completado
-void mostrar(); //Completado
-int sumaUtilidades(); 
+// Índice
+void llenado();           // Completado
+void llenadoAutomatico(); // Completado
+void mostrar();           // Completado
+int sumaUtilidades();
 int productoMasVendido();
 int productoMasCaro();
 int totalStock();
@@ -22,57 +22,64 @@ int productoMasUtilidad();
 float promedioDeVentas();
 int productoMenosVentas();
 void buscar();
-void barraProgreso(); //Extra
+void barraProgreso(); // Extra
 
-int main() {
+int main()
+{
     bool continuar = true;
     char opc;
-    while(continuar){
-        otro:
-        cout << endl << "Selecciona una opcion a ejecutar: " << endl;
+    while (continuar)
+    {
+    otro:
+        cout << endl
+             << "Selecciona una opcion a ejecutar: " << endl;
         cout << "1	Llenado\n2	Llenado Automatico\n3	Mostrar\n4	Suma de las Utilidades\n5	producto que mas se vende\n6	Producto mas caro\n7	Total de stock\n8	Producto con mas utilidad\n9	Promedio de ventas\n-.	Producto que menos se vende\nb.	Buscar un dato\ns.      salir" << endl;
         cin >> opc;
-        switch(opc){ //Menu de opciones
-            case '1':
-                llenado();
-                break;
-            case '2':
-                llenadoAutomatico();
-                break;
-            case '3':
-                mostrar();
-                break;
-            case '4':
-                break;
-            case '5':
-                break;
-            case '6':
-                break;
-            case '7':
-                break;
-            case '8':
-                break;
-            case '9':
-                break;
-            case '-':
-                break;
-            case 'b':
-                break;
-            case 's':
-                continuar = false;
-                break;
-            default:
-                cout << "Esa opcion no exite, intentalo de nuevo: " << endl;
-                goto otro;
+        switch (opc)
+        { // Menu de opciones
+        case '1':
+            llenado();
+            break;
+        case '2':
+            llenadoAutomatico();
+            break;
+        case '3':
+            mostrar();
+            break;
+        case '4':
+            break;
+        case '5':
+            break;
+        case '6':
+            break;
+        case '7':
+            break;
+        case '8':
+            break;
+        case '9':
+            break;
+        case '-':
+            break;
+        case 'b':
+            break;
+        case 's':
+            continuar = false;
+            break;
+        default:
+            cout << "Esa opcion no exite, intentalo de nuevo: " << endl;
+            goto otro;
         }
     }
 }
 
-void llenado() { //llenado con datos ingresados por el usuario
+void llenado()
+{ // llenado con datos ingresados por el usuario
     for (int i = 0; i <= F; i++)
-        for (int j = 0; j <= C; j++){
-            cout << "Producto: "<< i << ": ";
-            switch (j) {
+        for (int j = 0; j <= C; j++)
+        {
+            cout << "Producto: " << i << ": ";
+            switch (j)
+            {
             case 0:
                 cout << "Stock" << endl;
                 break;
@@ -90,65 +97,72 @@ void llenado() { //llenado con datos ingresados por el usuario
         }
 }
 
-void llenadoAutomatico(){ //Lenado automático con números aleatorios
+void llenadoAutomatico()
+{ // Lenado automático con números aleatorios
     barraProgreso();
     srand(time(NULL));
-    for (int i = 0; i <= F; i++) 
-        for (int j = 0; j <= C; j++){
-            tabla1[i][j] = 10 + rand()%20;
+    for (int i = 0; i <= F; i++)
+        for (int j = 0; j <= C; j++)
+        {
+            tabla1[i][j] = 10 + rand() % 20;
         }
 }
 
-void mostrar() { //Muestra la matriz
-    for (int i = 0; i <= F; i++){
+void mostrar()
+{ // Muestra la matriz
+    for (int i = 0; i <= F; i++)
+    {
         cout << endl;
-        for (int j = 0; j <= C; j++){
+        for (int j = 0; j <= C; j++)
+        {
             cout << tabla1[i][j] << "\t";
         }
     }
 }
-
-int sumaUtilidades(){
-    
+// hola
+int sumaUtilidades()
+{
 }
 
-int productoMasVendido(){
-
+int productoMasVendido()
+{
 }
 
-int productoMasCaro(){
-
+int productoMasCaro()
+{
 }
 
-int totalStock(){
-
+int totalStock()
+{
 }
 
-int productoMasUtilidad(){
-
+int productoMasUtilidad()
+{
 }
 
-float promedioDeVentas(){
-
+float promedioDeVentas()
+{
 }
 
-int productoMenosVentas(){
-
+int productoMenosVentas()
+{
 }
 
-void buscar(){
-
+void buscar()
+{
 }
 
-void barraProgreso() { //Extra, contiene una función para generar una barra de progreso
-    int segundos=1;
+void barraProgreso()
+{ // Extra, contiene una función para generar una barra de progreso
+    int segundos = 1;
     cout << "\t\t    Comenzando llenado automatico...\n";
-    for (int i=0; i <= 79; i++)
+    for (int i = 0; i <= 79; i++)
         cout << "_";
     cout << endl;
-    for (int i = 0; i <= 79; i++){
+    for (int i = 0; i <= 79; i++)
+    {
         cout << char(219);
-        Sleep(segundos*1000/80);
+        Sleep(segundos * 1000 / 80);
     }
     cout << "\nCompletado!" << endl;
 }
