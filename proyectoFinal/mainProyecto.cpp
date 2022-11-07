7cb9bc0a30e9d5d47434142ff90571791a99ee3f
Cristian Aragon <crisarag8.sal@gmail.com>
2022-11-07 10:30:43 -0600

    Proyecto Finalizado Beta1

--- proyectoFinal/mainProyecto.cpp
+++ proyectoFinal/mainProyecto.cpp
@@ -16,38 +16,27 @@ void llenado();           // Completado
 void llenadoAutomatico(); // Completado
 void mostrar();           // Completado
 int sumaUtilidades();     // Completado
-int productoMasVendido();
-int productoMenosVentas();
-int productoMasCaro();
-<<<<<<< HEAD
-int totalStock();
-int productoMasUtilidad();
-float promedioDeVentas();
+int productoMasVendido(); //Completado
+int productoMenosVentas(); //Completado
+int productoMasCaro(); //Completado
+int totalStock(); //Completado
+int productoMasUtilidad(); //Completado
+float promedioDeVentas(); // Completado
 void buscar(int dato); // Completado
 void barraProgreso();  // Extra
-=======
-int totalStock(); //Completado
-int productoMasUtilidad(); // Completado
-float promedioDeVentas(); // Completado
-void buscar(int dato); //Completado
-void barraProgreso(); // Extra
->>>>>>> 7427da5eebeaaf5a4934511e5c811e6c55be7fed
 
-int main()
-{
+int main() {
     int aux;
     float aux2;
     bool continuar = true;
     char opc;
-    while (continuar)
-    {
+    while (continuar) {
     otro:
         cout << endl
-             << "Selecciona una opcion a ejecutar: " << endl;
+            << "Selecciona una opcion a ejecutar: " << endl;
         cout << "1	Llenado\n2	Llenado Automatico\n3	Mostrar\n4	Suma de las Utilidades\n5	producto que mas se vende\n6	Producto mas caro\n7	Total de stock\n8	Producto con mas utilidad\n9	Promedio de ventas\n-.	Producto que menos se vende\nb.	Buscar un dato\ns.      salir" << endl;
         cin >> opc;
-        switch (opc)
-        { // Menu de opciones
+        switch (opc) { // Menu de opciones
         case '1':
             llenado();
             break;
@@ -62,8 +51,12 @@ int main()
             cout << "La suma de las utilidades es " << aux << endl;
             break;
         case '5':
+            aux = productoMasVendido();
+            cout << "El producto mas vendido es " << aux << endl;
             break;
         case '6':
+            aux = productoMasCaro();
+            cout << "El producto mas caro es " << aux << endl;
             break;
         case '7':
             aux = totalStock();
@@ -78,6 +71,8 @@ int main()
             cout << "El promedio de ventas es " << aux2 << endl;
             break;
         case '-':
+            aux = productoMenosVentas();
+            cout << "El producto menos vendido es " << aux << endl;
             break;
         case 'b':
             cout << "Ingresa el dato a buscar: " << endl;
@@ -94,14 +89,11 @@ int main()
     }
 }
 
-void llenado()
-{ // llenado con datos ingresados por el usuario
+void llenado() { // llenado con datos ingresados por el usuario
     for (int i = 0; i < F; i++)
-        for (int j = 0; j < C; j++)
-        {
+        for (int j = 0; j < C; j++) {
             cout << "Producto: " << i << ", " << j << ": ";
-            switch (j)
-            {
+            switch (j) {
             case 0:
                 cout << "Stock" << endl;
                 break;
@@ -120,139 +112,102 @@ void llenado()
     llenada = true;
 }
 
-void llenadoAutomatico()
-{ // Lenado automático con números aleatorios
+void llenadoAutomatico() { // Lenado automático con números aleatorios
     barraProgreso();
     srand(time(NULL));
     for (int i = 0; i < F; i++)
-        for (int j = 0; j < C; j++)
-        {
+        for (int j = 0; j < C; j++) {
             tabla[i][j] = 10 + rand() % 20;
         }
     llenada = true;
 }
 
-void mostrar()
-{ // Muestra la matriz
+void mostrar() { // Muestra la matriz
     char eleccion;
-    if (llenada)
-    {
-        for (int i = 0; i < F; i++)
-        {
+    if (llenada) {
+        for (int i = 0; i < F; i++) {
             cout << endl;
-            for (int j = 0; j < C; j++)
-            {
+            for (int j = 0; j < C; j++) {
                 cout << tabla[i][j] << "\t";
             }
         }
     }
-    else
-    {
+    else {
         cout << "La tabla aun no ha sido llenada" << endl;
     }
     cout << endl;
 }
 
-int sumaUtilidades()
-{
+int sumaUtilidades() {
     int aux = 0;
     int utilidades[F], suma = 0;
     int ventas[F][3] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
     for (int i = 0; i < F; i++)
-        for (int j = 1; j < 3; j++)
-        {
+        for (int j = 1; j < 3; j++) {
             ventas[i][j] = tabla[i][j];
         }
-    for (int i = 0; i < F; i++)
-    {
-        utilidades[i] = ventas[i][1] - ventas[i][2];
+    for (int i = 0; i < F; i++) {
+        utilidades[i] = ventas[i][2] - ventas[i][1];
     }
     for (int i = 0; i < F; i++)
         suma += utilidades[i];
     return suma;
 }
 
-int productoMasVendido()
-{ // Gera
-    int mv = 0, utilidades[F][3];
-    for (int i = 0; i < F; i++)
-    {
-        if (utilidades[i][3] > mv)
-        {
-            mv = i;
+int productoMasVendido() { // Gera
+    int mv = INT_MIN, ventas[F], posicion;
+    for (int i = 0; i < F; i++){
+        ventas[i] = tabla[i][3];
+    }
+    for (int i = 0; i < F; i++) {
+        if (ventas[i] > mv) {
+            mv = ventas[i];
+            posicion = i+1;
         }
     }
-    return mv;
+    return posicion;
 }
 
-int productoMenosVentas()
-{ // Gera
-    int mv = 10000, utilidades[F][3];
-    for (int i = 0; i < F; i++)
-    {
-        if (utilidades[i][3] < mv)
-        {
-            mv = i;
+int productoMenosVentas() { // Pendiente
+    int mv = INT_MAX, ventas[F], posicion;
+    for (int i = 0; i < F; i++){
+        ventas[i] = tabla[i][3];
+    }
+    for (int i = 0; i < F; i++) {
+        if (ventas[i] < mv) {
+            mv = ventas[i];
+            posicion = i+1;
         }
     }
-    return mv;
+    return posicion;
 }
 
-int productoMasCaro()
-{ // Gera
-    int mc = 0, utilidades[F][2];
-    for (int i = 0; i < F; i++)
-    {
-        if (utilidades[i][2] > mc)
-        {
-            mc = i;
+int productoMasCaro() { // Pendiente
+    int mc = INT_MIN, costo[F], posicion;
+    for (int i = 0; i < F; i++){
+        costo[i] = tabla[i][2];
+    }
+    for (int i = 0; i < F; i++) {
+        if (costo[i] > mc) {
+            mc = costo[i];
+            posicion = i+1;
         }
     }
-    return mc;
+    return posicion;
 }
 
-<<<<<<< HEAD
-int totalStock()
-{ // Ángel
-    int stock[F], suma;
-    suma = 0;
-    for (int i = 0; i < F; i++)
-=======
 int totalStock(){ //Ángel
 int stock[F], suma;
     suma=0;
     for(int i=0; i<F; i++)
         stock[i] = tabla[i][0];
     for (int i = 0; i<F; i++)
->>>>>>> 7427da5eebeaaf5a4934511e5c811e6c55be7fed
         suma += stock[i];
     return suma;
 }
 
-<<<<<<< HEAD
-int productoMasUtilidad()
-{ // Ángel
-    int utilidades[F], mayor;
-    mayor = 0;
-    for (int i = 0; i < F; i++)
-        if (utilidades[i] > mayor)
-            mayor = utilidades[i];
-    return mayor;
-}
-
-float promedioDeVentas()
-{ // Ángel
-    float ventas[F], promedio, suma;
-    suma = 0;
-    for (int i = 0; i < F; i++)
-    {
-        suma += ventas[i];
-    }
-    promedio = suma / F;
-    return promedio;
-=======
 int productoMasUtilidad(){ //Ángel
-    int utilidades[F] = {0, 0, 0, 0, 0}, mayor = INT_MIN;
+    int utilidades[F] = {0, 0, 0, 0, 0}, mayor = INT_MIN, posicion;
     int ventas[F][3] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
     for (int i = 0; i < F; i++)
         for (int j = 1; j < 3; j++){
@@ -264,10 +219,11 @@ int productoMasUtilidad(){ //Ángel
     }
     for (int i = 0; i < F; i++){
         if (utilidades[i] > mayor){
-            mayor = i+1;
+            mayor = utilidades[i];
+            posicion = i+1;
         }
     }
-    return mayor;
+    return posicion;
 }
 
 float promedioDeVentas(){ //Ángel
@@ -281,40 +237,33 @@ float promedioDeVentas(){ //Ángel
     }
     promedio = float(suma)/float(F);
     return promedio; 
->>>>>>> 7427da5eebeaaf5a4934511e5c811e6c55be7fed
 }
 
-void buscar(int dato)
-{ // Cristian
+void buscar(int dato) { // Cristian
     bool bandera = false;
     int posicionX, posicionY;
     for (int i = 0; i < F; i++)
         for (int j = 0; j < C; j++)
-            if (tabla[i][j] == dato)
-            {
+            if (tabla[i][j] == dato) {
                 bandera = true;
                 posicionX = i;
                 posicionY = j;
             }
-    if (bandera)
-    {
+    if (bandera) {
         cout << "Si se encontro el dato en la posicion " << posicionX << ", " << posicionY << endl;
     }
-    else
-    {
+    else {
         cout << "No se encontro el dato" << endl;
     }
 }
 
-void barraProgreso()
-{ // Extra, contiene una función para generar una barra de progreso
+void barraProgreso() { // Extra, contiene una función para generar una barra de progreso
     int segundos = 1;
     cout << "\t\t    Comenzando llenado automatico...\n";
     for (int i = 0; i <= 79; i++)
         cout << "_";
     cout << endl;
-    for (int i = 0; i <= 79; i++)
-    {
+    for (int i = 0; i <= 79; i++) {
         cout << char(219);
         Sleep(segundos * 1000 / 80);
     }
